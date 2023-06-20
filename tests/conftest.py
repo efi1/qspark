f1bb81e830c622e4b8143556af754c9b9a96ad3b
efi1 <efovadia@gmail.com>
2023-06-21 00:10:01 +0300

    - add type hints for functions (some were missing). - add two function which responsible for parameterizing some of the tests. - update the tests' cfg files to be complient with the parameterizing changes as added in conftest.py - Update test_requests.py with the parameterized vars.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Shared fixtures."""
+import copy
 import os # being used when evaluating settings
 import yaml
 from pathlib import Path  # being used in func: pytest_addoption
@@ -11,6 +12,10 @@ settings_items = [i for i in settings.__dir__() if not i.startswith('_')]
 
 
 def pytest_addoption(parser):
+    """
+    built-in pytest function to enable flags to be run via pytest
+        e.g.   python -m pytest -<added flag by this func> <flag val>
+    """
     for item in settings_items:
         try:
             value = eval(getattr(settings, item))
@@ -26,6 +31,9 @@ def test_name(request):
 
 @fixture(scope="session")
 def tests_raw_data(request):
+    """
+    get all the tests' common data
+    """
     raw_data = dict()
     for item in settings_items:
         raw_data[item] = request.config.getoption(F"--{item}")
@@ -44,6 +52,38 @@ def settings_data(tests_raw_data: dict) -> object:
 
 @fixture(scope="function")
 def cfg_data(test_name) -> object:
+    """
+    :param test_name: the test name
+    :return: test arguments only for non parametrized tests
+    """
+    test_name = test_name.split('[')[0]
+    cfg_name = F'{test_name}.cfg'
+    if not cfg_name.startswith('test_param'):
+        cfg_template_dir = eval(settings.cfg_tests_folder)
+        cfg_template_file = Path(cfg_template_dir).joinpath(cfg_name)
+        if cfg_template_file.exists():
+            with open(cfg_template_file, 'r') as cfg_data:
+                cfg_data_dict = yaml.safe_load(cfg_data)
+                return dict_to_obj(cfg_data_dict)
+
+
+@fixture(scope="session")
+def tests_client(settings_data: object) -> object:
+    """
+    :param settings_data: general data which required for the tests
+    :return: initiate the client and return its instance
+    """
+    tests_client = RequestesLocatesClient(rsrc_path=settings_data.resources_folder)
+    tests_client.load_data(settings_data.csv_file_name)
+    yield tests_client
+
+
+def load_test_params(test_name) -> object:
+    """
+    load all args which required only for the parametrized tests
+    :param test_name: the test name
+    :return: tests' arguments only for parametrized tests
+    """
     test_name = test_name.split('[')[0]
     cfg_name = F'{test_name}.cfg'
     cfg_template_dir = eval(settings.cfg_tests_folder)
@@ -51,12 +91,25 @@ def cfg_data(test_name) -> object:
     if cfg_template_file.exists():
         with open(cfg_template_file, 'r') as cfg_data:
             cfg_data_dict = yaml.safe_load(cfg_data)
-            return dict_to_obj(cfg_data_dict)
+            field_name = cfg_data_dict.get('field_name')
+            approved_locates = cfg_data_dict.get('approved_locates')
+            in_params = cfg_data_dict.get('in_params')
+            expected_params = cfg_data_dict.get('expected_params')
+            values = zip(in_params, expected_params, field_name, approved_locates)
+            parameterized_data = cfg_data_dict.get('parameterized_data')
+            parameterized_data[test_name]['values'] = values
+            return parameterized_data
 
 
-@fixture(scope="session")
-def tests_client(settings_data: object) -> object:
-    tests_client = RequestesLocatesClient(rsrc_path=settings_data.resources_folder)
-    tests_client.load_data(settings_data.csv_file_name)
-    yield tests_client
+def pytest_generate_tests(metafunc):
+    """
+    generate the parametrized args which rewuired for the parametrized tests.
+    :param metafunc: pytest built-in function
+    """
+    fct_name = metafunc.function.__name__
+    if fct_name.startswith('test_param'):
+        parameterized_data = load_test_params(fct_name)
+        if parameterized_data.get(fct_name):
+            params = parameterized_data[fct_name]
+            metafunc.parametrize(params["params"], params["values"])
 
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,78 +1,53 @@
-import pytest
 import logging
-
 
 logging.getLogger()
 
-in_params = [{"client_name": "Client1", 'symbol': "TTT", "number_of_locates_requested": 100},
-             {"client_name": "Client2", 'symbol': "TTT", "number_of_locates_requested": 200},
-             {"client_name": "Client3", 'symbol': "TTT", "number_of_locates_requested": 100},
-             {"client_name": "Client4", 'symbol': "TTT", "number_of_locates_requested": 100}]
 
-expected_params = [{'client_name': 'Client1', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100},
-                   {'client_name': 'Client2', 'symbol': 'TTT', 'req_locates': 200, 'approved_locates': 200},
-                   {'client_name': 'Client3', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100},
-                   {'client_name': 'Client4', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100}]
-
-
-@pytest.mark.parametrize("inp,expected", zip(in_params, expected_params))
-def test_devisible_by_100(inp, expected, settings_data, tests_client, cfg_data, request):
+def test_param_devisible_by_100(req_inp, req_out, settings_data, tests_client, field_name, approved_locates, request):
     """
     Verify that the approved locates are calculated correctly and should be proportional to the requested sum.
-    :param inp: customer request for locates of a specific symbol.
-    :param expected: server response (approved locates) for the customer request.
+    :param req_inp: customer request for locates of a specific symbol.
+    :param req_out: server response (approved locates) for the customer request.
     :param settings_data: data which is taken from ~tests/settings.py - being parsed by conftest.py
     :param tests_client: the software client which is being used for query the input data (csv with locates)
-    :param cfg_data: the test's arguments.
+    :param field_name: the field to be gathered from - in our scope we used field=symbol
+    :param approved_locates: the approved locate per each symbol.  e.g. {"ABC" : 480, "QQQ" : 445, "TTT" : 210.12}
     :param request: internal pytest object which is needed to get the iteration number.
     """
     # calc is to be run only at first iteration, since input doesn't change, there is no need to recalculate.
-    if request.node.name.split('[')[1].split('-')[0] == 'inp0':
-        tests_client.calc_approved_locates_by_client(cfg_data.field_name, cfg_data.approved_locates)
-    res = tests_client.request_locates(inp)
-    assert res == expected, F"wrong approved locates; total approved: {cfg_data.approved_locates} locates, expected:" \
-                            F" {expected}, found: {res}"
+    if request.node.name.split('[')[1].split('-')[0].endswith('inp0'):
+        tests_client.calc_approved_locates_by_client(field_name, approved_locates)
+    res = tests_client.request_locates(req_inp)
+    assert res == req_out, F"wrong approved locates; total approved: {approved_locates} locates, expected:" \
+                            F" {req_out}, found: {res}"
 
 
-expected_params = [{'client_name': 'Client1', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100},
-                   {'client_name': 'Client2', 'symbol': 'TTT', 'req_locates': 200, 'approved_locates': 199.326},
-                   {'client_name': 'Client3', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100},
-                   {'client_name': 'Client4', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 100}]
-
-
-@pytest.mark.parametrize("inp,expected", zip(in_params, expected_params))
-def test_not_divisible_by_100(inp, expected, tests_client, cfg_data, request):
+def test_param_not_divisible_by_100(req_inp, req_out, tests_client, field_name, approved_locates, request):
     """
     Verify that the approved locates are calculated correctly when total approved chunks per symbol isn't divisible by 100.
-    :param inp: customer request for locates of a specific symbol.
-    :param expected: server response (approved locates) for the customer request.
+    :param req_inp: customer request for locates of a specific symbol.
+    :param req_out: server response (approved locates) for the customer request.
     :param tests_client: the software client which is being used for query the input data (csv with locates)
-    :param cfg_data: the test's arguments.
+    :param field_name: the field to be gathered from - in our scope we used field=symbol
+    :param approved_locates: the approved locate per each symbol.  e.g. {"ABC" : 480, "QQQ" : 445, "TTT" : 210.12}
     :param request: internal pytest object which is needed to get the iteration number.
     """
-    if request.node.name.split('[')[1].split('-')[0] == 'inp0': # to be run only at first iteration only
-        tests_client.calc_approved_locates_by_client(cfg_data.field_name, cfg_data.approved_locates)
-    res = tests_client.request_locates(inp)
-    assert res == expected, F"wrong approved locates; total approved: {cfg_data.approved_locates} locates, expected:" \
-                            F" {expected}, found: {res}"
+    if request.node.name.split('[')[1].split('-')[0].endswith('inp0'):  # to be run only at first iteration only
+        tests_client.calc_approved_locates_by_client(field_name, approved_locates)
+    res = tests_client.request_locates(req_inp)
+    assert res == req_out, F"wrong approved locates; total approved: {approved_locates} locates, expected:" \
+                            F" {req_out}, found: {res}"
 
 
-expected_params = [{'client_name': 'Client1', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 49.326},
-                   {'client_name': 'Client2', 'symbol': 'TTT', 'req_locates': 200, 'approved_locates': 0},
-                   {'client_name': 'Client3', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 0},
-                   {'client_name': 'Client4', 'symbol': 'TTT', 'req_locates': 100, 'approved_locates': 0}]
-
-
-@pytest.mark.parametrize("inp,expected", zip(in_params, expected_params))
-def test_approved_less_than_100(inp, expected, tests_client, cfg_data, request):
+def test_param_approved_less_than_100(req_inp, req_out, tests_client, field_name, approved_locates, request):
     """
     Verify that the approved locates are calculated correctly when total approved is smaller a 100 chunk.
     """
-    if request.node.name.split('[')[1].split('-')[0] == 'inp0': # to be run only at first iteration only
-        tests_client.calc_approved_locates_by_client(cfg_data.field_name, cfg_data.approved_locates)
-    res = tests_client.request_locates(inp)
-    assert res == expected, F"wrong approved locates; total approved: {cfg_data.approved_locates} locates, expected:" \
-                            F" {expected}, found: {res}"
+    if request.node.name.split('[')[1].split('-')[0].endswith('inp0'):  # to be run only at first iteration only
+        tests_client.calc_approved_locates_by_client(field_name, approved_locates)
+    res = tests_client.request_locates(req_inp)
+    assert res == req_out, F"wrong approved locates; total approved: {approved_locates} locates, expected:" \
+                            F" {req_out}, found: {res}"
 
 
 def test_neg_missing_client_request(tests_client, cfg_data):
@@ -98,3 +73,4 @@ def test_neg_approved_bigger_than_requested(tests_client, cfg_data):
     tests_client.calc_approved_locates_by_client(cfg_data.field_name, cfg_data.approved_locates)
     res = str(tests_client.request_locates(cfg_data.request))
     assert res == cfg_data.approved, F"client request for {cfg_data.request['client_name']} shouldn't exist"
+
