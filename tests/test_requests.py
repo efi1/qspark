b51470a1916e8e349756e83386ef2dc2c77bdfce
efi1 <efovadia@gmail.com>
2023-06-20 10:16:09 +0300

    remove settings_data argument as it is redundant and not being used.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -40,12 +40,11 @@ expected_params = [{'client_name': 'Client1', 'symbol': 'TTT', 'req_locates': 10
 
 
 @pytest.mark.parametrize("inp,expected", zip(in_params, expected_params))
-def test_not_divisible_by_100(inp, expected, settings_data, tests_client, cfg_data, request):
+def test_not_divisible_by_100(inp, expected, tests_client, cfg_data, request):
     """
     Verify that the approved locates are calculated correctly when total approved chunks per symbol isn't divisible by 100.
     :param inp: customer request for locates of a specific symbol.
     :param expected: server response (approved locates) for the customer request.
-    :param settings_data: data which is taken from ~tests/settings.py - being parsed by conftest.py
     :param tests_client: the software client which is being used for query the input data (csv with locates)
     :param cfg_data: the test's arguments.
     :param request: internal pytest object which is needed to get the iteration number.
@@ -64,7 +63,7 @@ expected_params = [{'client_name': 'Client1', 'symbol': 'TTT', 'req_locates': 10
 
 
 @pytest.mark.parametrize("inp,expected", zip(in_params, expected_params))
-def test_approved_less_than_100(inp, expected, settings_data, tests_client, cfg_data, request):
+def test_approved_less_than_100(inp, expected, tests_client, cfg_data, request):
     """
     Verify that the approved locates are calculated correctly when total approved is smaller a 100 chunk.
     """
@@ -75,11 +74,10 @@ def test_approved_less_than_100(inp, expected, settings_data, tests_client, cfg_
                             F" {expected}, found: {res}"
 
 
-def test_neg_missing_client_request(settings_data, tests_client, cfg_data):
+def test_neg_missing_client_request(tests_client, cfg_data):
     """
     Verify that a correct error received when a non-valid request is made (a customer who hasn't requested locates,
     but checks later if they are approved).
-    :param settings_data: data which is taken from ~tests/settings.py - being parsed by conftest.py
     :param tests_client: the software client which is being used for query the input data (csv with locates)
     :param cfg_data: the test's arguments.
     """
@@ -88,12 +86,11 @@ def test_neg_missing_client_request(settings_data, tests_client, cfg_data):
     assert res == cfg_data.approved, F"client request for {cfg_data.request['client_name']} shouldn't exist"
 
 
-def test_neg_approved_bigger_than_requested(settings_data, tests_client, cfg_data):
+def test_neg_approved_bigger_than_requested(tests_client, cfg_data):
     """
     Verify that a correct error received when a non-valid request approval was made (the server approved more than
      was requested).
     but checks later if they are approved).
-    :param settings_data: data which is taken from ~tests/settings.py - being parsed by conftest.py
     :param tests_client: the software client which is being used for query the input data (csv with locates)
     :param cfg_data: the test's arguments.
     """
